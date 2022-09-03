c0d967aa3aa9e21de59e6ce90751ab953101495c
Ibrahim Abdulwahab <ibrahim.abdulwahab1997@gmail.com>
2022-09-03 13:21:07 +0300

    the reset is now a click

--- game.js
+++ game.js
@@ -8,13 +8,13 @@ window.addEventListener('load', function(){
     document.getElementById("boundary2").addEventListener("mouseenter", mouseEnter);
     document.getElementById("boundary3").addEventListener("mouseenter", mouseEnter);
     document.getElementById("boundary4").addEventListener("mouseenter", mouseEnter);
-    document.getElementById("start").addEventListener("mouseenter", mouseEnter2);
+    document.getElementById("start").addEventListener("click", onClick);
     function mouseEnter(){
         for (var i=0; i<boundaries.length-1;i++){
             document.getElementById("boundary"+i).className += " youlose";}
         document.getElementById("status").innerHTML= "You Lost"
     }
-    function mouseEnter2(){
+    function onClick(){
         for (var i=0; i<boundaries.length-1;i++){
             document.getElementById("boundary"+i).className = "boundary";}
     }
