08d822d0fed82bb7aebc88c6e44ec7914d5dfb1f
Ibrahim Abdulwahab <ibrahim.abdulwahab1997@gmail.com>
2022-09-03 12:47:50 +0300

    added a for loop that colors all the boundaries

--- game.js
+++ game.js
@@ -1,10 +1,11 @@
 window.addEventListener('load', function(){
     var boundaries=document.getElementsByClassName("boundary");
-    for (var i =0 ; i< boundaries.length; i++){
+    for (var i =0 ; i< boundaries.length-1; i++){
         boundaries[i].setAttribute("id","boundary"+i);
     }
-    document.getElementById("boundary0").className += " youlose";
-    
+    for (var i=0; i<boundaries.length-1;i++){
+        document.getElementById("boundary"+i).className += " youlose";
+    }
     var lol= document.getElementById("game");
     console.log(lol);
 })
