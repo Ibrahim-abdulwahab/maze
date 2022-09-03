48c5fdf66a7fbaa6255b34a97ff7f947580599fc
Ibrahim Abdulwahab <ibrahim.abdulwahab1997@gmail.com>
2022-09-03 12:44:05 +0300

    wrote the code that will change the maze's color

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function(){
     for (var i =0 ; i< boundaries.length; i++){
         boundaries[i].setAttribute("id","boundary"+i);
     }
-    //document.getElementById("boundary0").className = "youlose";
+    document.getElementById("boundary0").className += " youlose";
     
     var lol= document.getElementById("game");
     console.log(lol);
